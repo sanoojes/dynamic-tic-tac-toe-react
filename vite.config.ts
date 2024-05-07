5bf36fae14b8f5a2ca00fc7b031f0dc9f91deb5f
Sanooj Es <sanoojes6371@gmail.com>
2024-05-07 18:20:56 +0530

    feat: change base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/tic-tac-toe-react/",
+  base: "/dynamic-tic-tac-toe-react/",
   plugins: [react()],
 });
 
